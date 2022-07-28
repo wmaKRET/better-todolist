6b7e5c341e4fd2e9c5474a6d4dfa1c2b54537164
wmaKRET <wkret91.2@gmail.com>
2022-07-28 10:19:05 +0200

    Style TasksListMenu

--- src/components/tasks-list/TasksListMenu.js
+++ src/components/tasks-list/TasksListMenu.js
@@ -1,14 +1,32 @@
+import { MdKeyboardArrowDown } from "react-icons/md"
+
 const TasksListMenu = () => {
     return (
         <div className="task-list__menu">
-            <p>What needs to be done?</p>
+            <div className="task-list__menu-msg">
+                <MdKeyboardArrowDown size={28} />
+                <p id="task-list-msg">What needs to be done?</p>
+                <MdKeyboardArrowDown size={28} />
+            </div>
             <input
                 className="task-list__menu-input"
                 placeholder="Write here..."
             ></input>
-            <button className="task-list__menu-btn">ADD TASK</button>
-            <button className="task-list__menu-btn">CLEAR COMPLETED</button>
-            <button className="task-list__menu-btn">CLEAR ALL</button>
+            <button
+                className="task-list__menu-btn"
+            >
+                ADD TASK
+            </button>
+            <button
+                className="task-list__menu-btn"
+            >
+                CLEAR COMPLETED
+            </button>
+            <button
+                className="task-list__menu-btn"
+            >
+                CLEAR ALL
+            </button>
         </div>
     )
 }
