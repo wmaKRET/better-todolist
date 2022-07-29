ba48d11e44629e5e1bbe816c0d67e3350168bfba
wmaKRET <wkret91.2@gmail.com>
2022-07-29 10:30:51 +0200

    Add basic styling for Lists component

--- src/components/lists/Lists.js
+++ src/components/lists/Lists.js
@@ -1,21 +1,34 @@
 const Lists = () => {
-  return (
-    <div className="lists">
-        <div className="lists__menu">
-            <input placeholder="List name"></input>
-            <button>+</button>
-        </div>
-        <div className="lists__list">
-            <div className="lists__list-item">
-                <p>list name</p>
-                <p>5</p>
-                <p>/</p>
-                <p>2</p>
-                <p>x</p>
+    return (
+        <div className="lists">
+            <div className="lists__menu">
+                <input
+                    className="lists__menu-input"
+                    placeholder="List name"
+                ></input>
+                <button
+                    className="lists__menu-btn"
+                >
+                    +
+                </button>
+            </div>
+            <div className="lists__list">
+                <div className="lists__list-item">
+                    <div className="lists__list-item__details">
+                        <p>5</p>
+                        <p>/</p>
+                        <p>2</p>
+                    </div>
+                    <div className="lists__list-item__name">
+                        <p>list name</p>
+                    </div>
+                    <div className="lists__list-item__delete">
+                        <p>X</p>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Lists
