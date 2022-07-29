bf092611aed7094772f70e8425fd0e90de68b3c1
wmaKRET <wkret91.2@gmail.com>
2022-07-29 14:03:49 +0200

    Move list "logic" up (to parent - App.js)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
+import { useState } from "react"
 import Lists from "./components/lists/Lists"
 import Socials from "./components/socials/Socials"
 import Tasks from "./components/tasks/Tasks"
 import "./scss/app.scss"
 
-const App = () => (
-  <>
-    <Socials />
-    <div className="container">
-      <Lists />
-      <Tasks />
-    </div>
-  </>
-)
+const App = () => {
+  const [listsArray, setListsArray] = useState([])
+
+  const createList = (inputValue) => ({
+    id: Date.now().toString(),
+    value: inputValue,
+    tasks: []
+  })
+
+  const addList = (inputValue) => {
+    setListsArray(prevLists => [...prevLists, createList(inputValue)])
+  }
+
+  const deleteList = (listID) => {
+    setListsArray(prevLists => prevLists
+      .filter(list => list.id != listID))
+  }
+
+  return (
+    <>
+      <Socials />
+      <div className="container">
+        <Lists 
+          listsArray={listsArray}
+          addList={addList}
+          deleteList={deleteList}
+        />
+        <Tasks 
+        
+        />
+      </div>
+    </>
+  )
+}
 
 export default App
--- src/components/lists/Lists.js
+++ src/components/lists/Lists.js
@@ -1,26 +1,7 @@
-import { useState } from "react"
 import ListsList from "./ListsList"
-
 import ListsMenu from "./ListsMenu"
 
-const Lists = () => {
-    const [listsArray, setListsArray] = useState([])
-
-    const createList = (inputValue) => ({
-        id: Date.now().toString(),
-        value: inputValue,
-        tasks: []
-    })
-
-    const addList = (inputValue) => {
-        setListsArray(prevLists => [...prevLists, createList(inputValue)])
-    }
-
-    const deleteList = (listID) => {
-        setListsArray(prevLists => prevLists
-            .filter(list => list.id != listID))
-    }
-
+const Lists = ({ listsArray, addList, deleteList}) => {
     return (
         <div className="lists">
             <ListsMenu 
